8ebfd4dad43a2db9d8aa7b3e40c3906d9ef7a1f6
Chepkemboi <Chepkemboirabecca@gmail.com>
2023-05-04 23:10:38 +0300

    done py classes

---
+++ __pycache__/car.py
@@ -0,0 +1,15 @@
+# Car
+
+class Car:
+    car = "Toyota"
+
+
+def __init__(self,color, make, model,car):
+    self.color = color
+    self.make = make
+    self.model = model
+    self.car = car
+
+
+def my_car():
+    return f"The {self.color},{self.model} car of {self.make}is beautiful "   
---
+++ fruits.py
@@ -0,0 +1,15 @@
+# class fruit
+
+class Fruit:
+
+    is_fruit = True
+    
+    def __init__(self, name, color, is_sweet):
+
+        self.name = name
+        self.color = color
+        self.is_sweet = is_sweet
+
+
+def my_fruit():
+    return f"{self.name} that has {self.color} is very sweet "        
